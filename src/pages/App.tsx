9d5b8452c6a9cc59579df45988a6f84131d17e69
Sabrina Poderis <act172@einstein.br>
2025-02-10 19:16:00 -0300

    refactor: restart project from 0

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,19 +1,9 @@
-import NavBar from '../components/NavBar';
-import MainHero from '../sections/MainHero';
-import Skills from '../sections/Skills';
-import Projects from '../sections/Projects';
-import Footer from '../components/Footer';
 import React from 'react';
+import Container from '../components/Container';
 
 function Home() {
   return (
-    <div>
-      <NavBar></NavBar>
-      <MainHero></MainHero>
-      <Skills></Skills>
-      <Projects></Projects>
-      <Footer></Footer>
-    </div>
+    <Container>teste</Container>
   );
 }
 
