8ef8ad58a50040a683edc6ecffab27e13dd0e98c
Sabrina-Poderis <sabrina.poderis@gmail.com>
2025-03-19 14:15:12 -0300

    refactor: images folder

---
+++ src/assets/images/index.ts
@@ -0,0 +1,11 @@
+import ImageCollectionType from '../../ts/types/ImageCollectionType'
+import DefaultAvatarProfileIcon from './default-avatar-profile-icon.jpg'
+
+const images : ImageCollectionType = {
+    DefaultAvatarProfileIcon: {
+        image: DefaultAvatarProfileIcon,
+        textAlt: 'aaa'
+    }
+}
+
+export default images
