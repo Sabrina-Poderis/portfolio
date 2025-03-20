17f1f2dbface356396d5c805fc332330a6779d9a
Sabrina-Poderis <sabrina.poderis@gmail.com>
2025-03-19 22:16:01 -0300

    tirando o i18n pq da dando ruim

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import PortfolioHero from "@/sections/PortfolioHero";
 import { getDictionary } from "./dictionaries";
 
 const HomePage = async () => {
-  const lang = navigator.language as LocaleEnum || LocaleEnum.PORTUGUESE;
+  const lang = LocaleEnum.PORTUGUESE;
   const dict = await getDictionary(lang);
 
   return (
