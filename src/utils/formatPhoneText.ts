2a33d511e5c9bb140a3d83d6baeca714d3d89bde
Zabrina <Sabrina-Poderis@users.noreply.github.com>
2026-09-07 21:24:17 -0300

    feat: remove project

--- src/utils/formatPhoneText.ts
+++
@@ -1,8 +0,0 @@
-// Entrada: "5511912341234"
-// Saída: "11 9 1234-1234"
-
-const formatPhoneText = (phone: string) => {
-    return phone.replace(/^55(\d{2})(\d)(\d{4})(\d{4})$/, "($1) $2 $3-$4");
-}
-
-export default formatPhoneText
