e20912f21652989f3c78775591ae68a97f7f2b9b
Zabrina <Sabrina-Poderis@users.noreply.github.com>
2026-09-14 20:38:02 -0300

    fix: path

--- src/game/scenes/PortfolioScene.ts
+++ src/game/scenes/PortfolioScene.ts
@@ -23,7 +23,7 @@ export class PortfolioScene extends Phaser.Scene {
 
   preload(): void {
     this.load.tilemapTiledJSON("studio-01", `${import.meta.env.BASE_URL}maps/studio-01.json`);
-    this.load.image("player", `${import.meta.env.BASE_URL}assets/player2.svg`);
+    this.load.image("player", `${import.meta.env.BASE_URL}assets/player.svg`);
   }
 
   init(): void {
